c355bf95aecf5337381e34386cd20214ef19dd72
huynhduyquy2001 <huynhduyquy2001@gmail.com>
2024-08-12 13:42:29 +0700

    update ui

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
+import awesome_notifications_core
 import flutter_local_notifications
 import path_provider_foundation
 import rive_common
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  AwesomeNotificationsCorePlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
