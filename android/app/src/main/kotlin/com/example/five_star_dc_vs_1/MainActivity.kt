39f14cfa837e9b6f832fd76eaa1c32b7498158a7
huynhduyquy2001 <huynhduyquy2001@gmail.com>
2024-08-12 10:44:26 +0700

    update ui

---
+++ android/app/src/main/kotlin/com/example/five_star_dc_vs_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.five_star_dc_vs_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_local_notifications
+import path_provider_foundation
+import rive_common
+import sqflite
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
